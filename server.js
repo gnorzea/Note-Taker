99bc05848a1a984b880d416d889d34efe33723cc
Thunnder <gnorzea@gmail.com>
2022-08-02 07:11:29 -0400

    post/validatenote

--- server.js
+++ server.js
@@ -11,11 +11,45 @@ app.use(express.static('./Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes",(req,res) => {
+app.get("/notes", (req, res) => {
     res.json(notes);
 })
+
+app.post('/notes', (req, res) => {
+
+    req.body.id = notes.notes.length.toString();
+    
+    if (!validateNote(req.body)){
+        res.status(400).send('Your note is  not properly formatted');
+    }else {
+        const note = createNewNote(req.body, notes.notes)
+        res.json(note);
+    }
+        
+   
+});
+
+function createNewNote(body, notes) {
+    const note = body;
+    notes.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notes }, null, 2)
+    );
+    return note;
+}
+
+function validateNote(note) {
+if (!note.title || typeof note.title !== 'string') {
+    return false;
+}
+if (!note.text || typeof note.text !== 'string') {
+    return false;
+}
+return true;
+}
 
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
-  });
+});
